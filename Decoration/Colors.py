7759909aada0f40e1fbe14331e28b0124e3a82a0
Heretic <adamboulaaz92@gmail.com>
2026-07-22 01:33:31 +0100

    Add files via upload

---
+++ Decoration/Colors.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# ANSI color escape codes
+
+RESET = "\033[0m"
+GREY = "\033[90m"
+DIM = '\033[2m'
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+RESETBG = "\033[49m"
+BLACKBG = "\033[40m"
+REDBG = "\033[41m"
+GREENBG = "\033[42m"
+YELLOWBG = "\033[43m"
+BLUEBG = "\033[44m"
+MAGENTABG = "\033[45m"
+CYANBG = "\033[46m"
+WHITEBG = "\033[47m"
+ORANGE = "\033[38;2;255;165;0m"
+PURPLE = '\033[95m'
